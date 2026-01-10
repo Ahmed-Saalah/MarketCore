4f819b3a3a025bf55a339733db13d0f2258cfab8
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2026-01-10 19:25:27 +0200

    Add user created event publisher

---
+++ src/Services/Auth/Auth.API/Messages/UserCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Auth.API.Messages;
+
+public sealed record UserCreatedEvent(
+    int UserId,
+    string Username,
+    string Email,
+    string? PhoneNumber,
+    string DisplayName,
+    string? Role
+);
