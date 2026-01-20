65516dc263577097d4990bbd773c00b5413ddf09
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2026-01-20 14:07:25 +0200

    feat(Catalog): get product by id

---
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProductById.cs
@@ -0,0 +1,49 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities.Products;
+using Core.Domain.Errors;
+using Core.Domain.Response;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Features.Products;
+
+public sealed class GetProductById
+{
+    public sealed record Request(Guid Id) : IRequest<Response>;
+
+    public sealed class Response : Result<Product>
+    {
+        public static implicit operator Response(Product success) => new() { Value = success };
+
+        public static implicit operator Response(DomainError error) => new() { Error = error };
+    }
+
+    public sealed class Validator : AbstractValidator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(r => r.Id).NotEmpty().WithMessage("Product Id must be provided.");
+        }
+    }
+
+    public sealed class Handler(CatalogDbContext context, IValidator<Request> validator)
+        : IRequestHandler<Request, Response>
+    {
+        public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                return new ValidationError(validationResult.Errors);
+            }
+
+            var product = await context
+                .Products.Include(p => p.Images)
+                .Include(p => p.Attributes)
+                .FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+            return product is not null ? product : new NotFound("Product not found");
+        }
+    }
+}
