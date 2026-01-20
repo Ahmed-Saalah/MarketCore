d77be829aefc66b690cfe6c97e9efd2b60a593b8
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2026-01-20 14:07:04 +0200

    Feat(Catalog): Get category by id

---
+++ src/Services/Catalog/Catalog.API/Features/Categories/GetCategoryById.cs
@@ -0,0 +1,48 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities.Categories;
+using Core.Domain.Errors;
+using Core.Domain.Response;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Features.Categories;
+
+public sealed class GetCategoryById
+{
+    public sealed record Request(Guid Id) : IRequest<Response>;
+
+    public sealed class Response : Result<Category>
+    {
+        public static implicit operator Response(Category success) => new() { Value = success };
+
+        public static implicit operator Response(DomainError error) => new() { Error = error };
+    }
+
+    public sealed class Validator : AbstractValidator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(r => r.Id).NotEmpty().WithMessage("Category Id must be provided.");
+        }
+    }
+
+    public sealed class Handler(CatalogDbContext context, IValidator<Request> validator)
+        : IRequestHandler<Request, Response>
+    {
+        public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                return new ValidationError(validationResult.Errors);
+            }
+
+            var product = await context
+                .Categories.Include(c => c.Products)
+                .FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+
+            return product is not null ? product : new NotFound("Category not found");
+        }
+    }
+}
