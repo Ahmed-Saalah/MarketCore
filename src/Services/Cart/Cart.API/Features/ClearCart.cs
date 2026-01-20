19cacc09a600ff0b222a7ece4396f8ad59f0f45c
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2026-01-20 16:20:58 +0200

    Feat(Cart): Clear cart API

---
+++ src/Services/Cart/Cart.API/Features/ClearCart.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Claims;
+using Cart.API.Data;
+using Cart.API.Extensions;
+using Cart.API.Messages;
+using Core.Domain.Abstractions;
+using Core.Domain.Errors;
+using Core.Domain.Response;
+using Core.Messaging;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cart.API.Features;
+
+public sealed class ClearCart
+{
+    public sealed record Request(Guid CartId, Guid? UserId) : IRequest<Response>;
+
+    public sealed class Response : Result<bool>
+    {
+        public static implicit operator Response(bool success) => new() { Value = success };
+
+        public static implicit operator Response(DomainError error) => new() { Error = error };
+    }
+
+    public sealed class Handler(ICartRepository repository, IEventPublisher eventPublisher)
+        : IRequestHandler<Request, Response>
+    {
+        public async Task<Response> Handle(Request request, CancellationToken ct)
+        {
+            Entities.Cart? cart = await repository.GetCartAsync(request.CartId, ct);
+            if (cart is null && request.UserId.HasValue)
+            {
+                cart = await repository.GetCartByUserIdAsync(request.UserId.Value, ct);
+            }
+
+            if (cart is null)
+            {
+                return new NotFound("Cart not found");
+            }
+
+            cart.Items.Clear();
+            cart.UpdatedAt = DateTime.UtcNow;
+            await repository.StoreCartAsync(cart, ct);
+
+            await eventPublisher.PublishAsync(
+                new CartClearedEvent(cart.Id, cart.UserId, cart.StoreId, cart.UpdatedAt),
+                "Cart.CartClearedEvent",
+                ct
+            );
+
+            return true;
+        }
+    }
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void Map(IEndpointRouteBuilder app)
+        {
+            app.MapDelete(
+                    "/api/cart/{cartId}/clear",
+                    async (
+                        [FromRoute] Guid cartId,
+                        [FromRoute] Guid productId,
+                        IMediator mediator,
+                        ClaimsPrincipal user
+                    ) =>
+                    {
+                        var response = await mediator.Send(new Request(cartId, user.GetUserId()));
+                        return response.ToHttpResult();
+                    }
+                )
+                .WithTags("Cart")
+                .WithName("ClearCart");
+        }
+    }
+}
---
+++ src/Services/Cart/Cart.API/Messages/CartClearedEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace Cart.API.Messages;
+
+public sealed class CartClearedEvent(Guid CartId, Guid? UserId, Guid? StoreId, DateTime Timestamp);
