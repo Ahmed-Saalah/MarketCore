e7f34f4d1f0c55791181f823643e1d0f574d1311
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2026-01-22 22:19:01 +0200

    feat(api-gw): Add YARP

--- src/Gateways/ApiGateway/Program.cs
+++ src/Gateways/ApiGateway/Program.cs
@@ -1,6 +1,37 @@
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("reverseProxy.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+            factory: partition => new FixedWindowRateLimiterOptions
+            {
+                AutoReplenishment = true,
+                PermitLimit = 10,
+                Window = TimeSpan.FromSeconds(10),
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 5,
+            }
+        )
+    );
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("CorsPolicy");
+app.UseRateLimiter();
+app.MapReverseProxy();
 
 app.Run();
