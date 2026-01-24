1fcc00494a2090b61de28b1ebcf46dd94dceec07
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2026-01-24 19:23:35 +0200

    refactor(store): enhance extension methods

---
+++ src/Services/Store/Store.API/Extensions/DataExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.API.Data;
+
+namespace Store.API.Extensions;
+
+public static class DataExtensions
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<StoreDbContext>(options => options.UseNpgsql(connectionString));
+
+        return services;
+    }
+}
---
+++ src/Services/Store/Store.API/Extensions/MessaginExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Core.Messaging;
+using Store.API.Handlers;
+
+namespace Store.API.Extensions;
+
+public static class MessaginExtensions
+{
+    public static IServiceCollection AddMessaging(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.Configure<RabbitMqOptions>(configuration.GetSection(RabbitMqOptions.SectionName));
+
+        services.AddMessageBroker();
+
+        services.AddRabbitMqEventConsumer(
+            events: (
+                typeof(UserCreatedEventHandler.Event),
+                typeof(UserCreatedEventHandler.Handler),
+                "Auth.UserCreatedEvent"
+            )
+        );
+
+        return services;
+    }
+}
